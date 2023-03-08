817225efedf4284fac2acb9ae3e7a9a5b86c5ada
Kunwo-Sim <rjsdntlsdj@gmail.com>
2023-03-08 20:51:18 +0900

    [Add] vector 회전 기능 추가

--- WinAPI/Client/kwMath.h
+++ WinAPI/Client/kwMath.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <math.h>
+#define PI 3.141592
 
 struct Vector2
 {
@@ -16,18 +18,21 @@ struct Vector2
 	{
 
 	}
+
 	Vector2(int x, int y)
 		: x(x)
 		, y(y)
 	{
 
 	}
+
 	Vector2(float x, float y)
 		: x(x)
 		, y(y)
 	{
 
 	}
+
 	Vector2(const Vector2&) = default;
 	Vector2& operator=(const Vector2&) = default;
 
@@ -50,6 +55,15 @@ struct Vector2
 		return temp;
 	}
 
+	Vector2 operator*(const Vector2& other)
+	{
+		Vector2 temp;
+		temp.x = x * other.x;
+		temp.y = y * other.y;
+
+		return temp;
+	}
+
 	void operator+=(const Vector2& other)
 	{
 		x += other.x;
@@ -61,4 +75,36 @@ struct Vector2
 		x -= other.x;
 		y -= other.y;
 	}
+
+	float Length()
+	{
+		return sqrtf(x * x + y * y);
+	}
+
+	Vector2& Normalize()
+	{
+		float length = Length();
+		x /= length;
+		y /= length;
+
+		return *this;
+	}
 };
+
+namespace ya::math
+{
+	//float x = dir.x * cosf(PI / 5.0f) - dir.y * sinf(PI / 5.0f);
+	//float y = dir.x * sinf(PI / 5.0f) + dir.y * cosf(PI / 5.0f);
+
+	inline static Vector2 Rotate(Vector2 vector, float degree)
+	{
+		float radian = (degree / 180.0f) * PI;
+		vector.Normalize();
+
+		float x = vector.x * cosf(radian) - vector.y * sinf(radian);
+		float y = vector.x * sinf(radian) + vector.y * cosf(radian);
+		//atan
+		//atan();
+		return Vector2(x, y);
+	}
+}
